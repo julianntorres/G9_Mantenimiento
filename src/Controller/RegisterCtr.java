1a466dd7e270393bc825030754084d7fb01a02ff
Alexander-Fernando <alexander.glzprd@hotmail.com>
2023-06-01 12:36:38 -0500

    Refactorizacion (Long Method) - Aplicando EXtract Method en actionPermormed.

--- src/Controller/RegisterCtr.java
+++ src/Controller/RegisterCtr.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controller;
 
 import Interface.RegisterUser;
@@ -13,10 +9,6 @@ import Model.UserSession;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author ramos
- */
 public class RegisterCtr implements ActionListener {
     RegisterUser registerui;
     private UserSession session;
@@ -26,78 +18,75 @@ public class RegisterCtr implements ActionListener {
         registerui = new RegisterUser();
         registerui.btnRegister.addActionListener(this);
     }
-
+    
+     // REFACTORIZACIÓN actionPerformed Method - LONG METHOD - ALEXANDER GONZALES
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.registerui.btnRegister) {
-            int permisos_cmb;
-            boolean isValidate;
-            String nombre, apellido, pass, permisos_string = "";
-            nombre = this.registerui.txt_nombre.getText().trim();
-            apellido = this.registerui.txt_apellido.getText().trim();
-            pass = this.registerui.txt_contraseña.getText().trim();
-            permisos_cmb = registerui.cmb_niveles.getSelectedIndex() + 1;
-            if (permisos_cmb == 1) {
-                permisos_string = "Administrador";
-            } else if (permisos_cmb == 2) {
-                permisos_string = "Trabajador";
-            }
-            isValidate = this.validar(nombre, apellido, pass);
-            if (isValidate) {
+            String nombre = registerui.txt_nombre.getText().trim();
+            String apellido = registerui.txt_apellido.getText().trim();
+            String pass = registerui.txt_contraseña.getText().trim();
+            int permisos_cmb = registerui.cmb_niveles.getSelectedIndex() + 1;
+            
+            if (validateFields(nombre, apellido, pass)) {
+                String permisos_string = getPermisosString(permisos_cmb);
+                String sql = "";
+
                 if (permisos_string.equalsIgnoreCase("Administrador")) {
-                    System.out.println("hola soy un admin validado");
-                    String sql = String.format("INSERT INTO `admins`(`name_admin`, `ap_admin`, `pass_admin`) VALUES ('%s','%s','%s')", nombre, apellido, pass);
-                    System.out.println(sql);
-                    JOptionPane.showMessageDialog(null, "Administrador registrado con exito", "Mensaje del sistema", 1);
-                    try {
-                        new ConnectionPool().makeUpdate(sql);
-
-                        // Registrar en la tabla de auditoría
-                        int adminId = session.getId();
-                        String description = String.format("Registro de usuario: Nombre: %s, Apellido: %s, Tipo: Administrador", nombre, apellido);
-                        insertAuditLog(adminId, description);
-
-                    } catch (SQLException ex) {
-                        System.out.println(ex);
-                    }
+                    System.out.println("Hola, soy un admin validado");
+                    sql = String.format("INSERT INTO `admins`(`name_admin`, `ap_admin`, `pass_admin`) VALUES ('%s','%s','%s')", nombre, apellido, pass);
+                    JOptionPane.showMessageDialog(null, "Administrador registrado con éxito", "Mensaje del sistema", 1);
+                } else if (permisos_string.equalsIgnoreCase("Trabajador")) {
+                    System.out.println("Hola, soy un trabajador validado");
+                    sql = String.format("INSERT INTO `users`(`name_user`, `ap_user`, `pass_user`) VALUES ('%s','%s','%s')", nombre, apellido, pass);
+                    JOptionPane.showMessageDialog(null, "Trabajador registrado con éxito", "Mensaje del sistema", 1);
                 }
-                if (permisos_string.equalsIgnoreCase("Trabajador")) {
-                    System.out.println("hola soy un trabajador validado");
-                    String sql = String.format("INSERT INTO `users`(`name_user`, `ap_user`, `pass_user`) VALUES ('%s','%s','%s')", nombre, apellido, pass);
-                    JOptionPane.showMessageDialog(null, "Trabajador registrado con exito", "Mensaje del sistema", 1);
-                    try {
-                        new ConnectionPool().makeUpdate(sql);
 
-                        // Registrar en la tabla de auditoría
-                        int adminId = session.getId();
-                        String description = String.format("Registro de usuario: Nombre: %s, Apellido: %s, Tipo: Trabajador", nombre, apellido);
-                        insertAuditLog(adminId, description);
+                try {
+                    new ConnectionPool().makeUpdate(sql);
 
-                    } catch (SQLException ex) {
-                        System.out.println(ex);
-                    }
+                    // Registrar en la tabla de auditoría
+                    int adminId = session.getId();
+                    String description = String.format("Registro de usuario: Nombre: %s, Apellido: %s, Tipo: %s", nombre, apellido, permisos_string);
+                    insertAuditLog(adminId, description);
+
+                } catch (SQLException ex) {
+                    System.out.println(ex);
+                    JOptionPane.showMessageDialog(null, "Ocurrió un error al auditar el usuario", "Mensaje del sistema", 0);
                 }
             }
         }
     }
 
-    public boolean validar(String nombre, String apellido, String pass) {
-        int numValidate = 0;
+    private boolean validateFields(String nombre, String apellido, String pass) {
         if (nombre.equals("")) {
-            this.registerui.txt_nombre.setBackground(Color.red);
-            numValidate++;
+            registerui.txt_nombre.setBackground(Color.red);
+            return false;
         }
 
         if (apellido.equals("")) {
-            this.registerui.txt_apellido.setBackground(Color.red);
-            numValidate++;
+            registerui.txt_apellido.setBackground(Color.red);
+            return false;
         }
 
         if (pass.equals("")) {
-            this.registerui.txt_contraseña.setBackground(Color.red);
-            numValidate++;
+            registerui.txt_contraseña.setBackground(Color.red);
+            return false;
         }
-        return numValidate <= 0;
+
+        return true;
+    }
+
+    
+    //Nuevo método - Extract Method
+    private String getPermisosString(int permisos_cmb) {
+        if (permisos_cmb == 1) {
+            return "Administrador";
+        } else if (permisos_cmb == 2) {
+            return "Trabajador";
+        }
+        
+        return "";
     }
 
     private void insertAuditLog(int adminId, String description) {
@@ -106,7 +95,7 @@ public class RegisterCtr implements ActionListener {
             new ConnectionPool().makeUpdate(sql);
         } catch (SQLException ex) {
             System.out.println(ex);
-            JOptionPane.showMessageDialog(null, "Ocurrio un error al auditar el usuario", "Mensaje del sistema", 0);
+            JOptionPane.showMessageDialog(null, "Ocurrió un error al auditar el usuario", "Mensaje del sistema", 0);
         }
-}
+    }
 }
